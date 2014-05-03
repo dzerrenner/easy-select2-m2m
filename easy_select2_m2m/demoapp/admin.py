bd2a594707868e7f0fda3d5181be0bf1944850d5
asyncee <n10101010@gmail.com>
2014-05-03 18:19:56 +0400

    Fixed broken indentation

--- easy_select2_m2m/demoapp/admin.py
+++ easy_select2_m2m/demoapp/admin.py
@@ -1,62 +1,62 @@
-        from django.contrib import admin
-        from django import forms
-        from easy_select2 import Select2TextInput
-        from demoapp.models import Tag, Parent
+from django.contrib import admin
+from django import forms
+from easy_select2 import Select2TextInput
+from demoapp.models import Tag, Parent
 
 
-        class ParentForm(forms.ModelForm):
-            def __init__(self, *args, **kwargs):
-                super(ParentForm, self).__init__(*args, **kwargs)
+class ParentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ParentForm, self).__init__(*args, **kwargs)
 
-                def get_choices():
-                    values = Tag.objects.values_list('name', flat=True).distinct().order_by('name')
-                    return [x for x in values]
+        def get_choices():
+            values = Tag.objects.values_list('name', flat=True).distinct().order_by('name')
+            return [x for x in values]
 
-                # set select2attrs as stated in https://github.com/asyncee/django-easy-select2/issues/4
-                self.fields['tags'].widget = Select2TextInput(
-                    select2attrs={
-                        'tags': get_choices(),
-                    },
-                )
-                self.initial = {
-                    'tags': ', '.join([unicode(t) for t in self.instance.tags.all()]),
-                }
+        # set select2attrs as stated in https://github.com/asyncee/django-easy-select2/issues/4
+        self.fields['tags'].widget = Select2TextInput(
+            select2attrs={
+                'tags': get_choices(),
+            },
+        )
+        self.initial = {
+            'tags': ', '.join([unicode(t) for t in self.instance.tags.all()]),
+        }
 
-                # remove the "Use Ctrl for multiple selections" help text
-                self.fields['tags'].help_text = ""
+        # remove the "Use Ctrl for multiple selections" help text
+        self.fields['tags'].help_text = ""
 
-            def full_clean(self):
-                #
-                # Tags input should be a list of ids (ints), but for now
-                # it is list of single unicode strings, so we should convert
-                # it to correct format.
-                #
-                # Althought this approach is working, it is better to create
-                # custom field type that will do appropriate conversions.
-                #
-                tags_names = self.data.get('tags')
-                if tags_names is not None:
-                    tags_names = tags_names.split(',')
+    def full_clean(self):
+        #
+        # Tags input should be a list of ids (ints), but for now
+        # it is list of single unicode strings, so we should convert
+        # it to correct format.
+        #
+        # Althought this approach is working, it is better to create
+        # custom field type that will do appropriate conversions.
+        #
+        tags_names = self.data.get('tags')
+        if tags_names is not None:
+            tags_names = tags_names.split(',')
 
-                    tags_ids = []
-                    for name in tags_names:
-                        try:
-                            # It is possible to use get_or_create here.
-                            t = Tag.objects.get(name=name)
-                        except Tag.DoesNotExist:
-                            pass
-                        else:
-                            tags_ids.append(unicode(t.id))
+            tags_ids = []
+            for name in tags_names:
+                try:
+                    # It is possible to use get_or_create here.
+                    t = Tag.objects.get(name=name)
+                except Tag.DoesNotExist:
+                    pass
+                else:
+                    tags_ids.append(unicode(t.id))
 
-                    self.data['tags'] = tags_ids
+            self.data['tags'] = tags_ids
 
-                super(ParentForm, self).full_clean()
-
-
-        class ParentAdmin(admin.ModelAdmin):
-            form = ParentForm
-
-        admin.site.register(Parent, ParentAdmin)
+        super(ParentForm, self).full_clean()
 
 
-        admin.site.register(Tag)
+class ParentAdmin(admin.ModelAdmin):
+    form = ParentForm
+
+admin.site.register(Parent, ParentAdmin)
+
+
+admin.site.register(Tag)
